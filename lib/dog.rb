2c63de8f3f051ad99ce907f13250c6ee4e1136b4
Jacob Manley <jbmanley45@gmail.com>
2022-09-06 11:16:44 -0700

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-
+    # setter - writes
+    def breed=(breed)
+        @breed = breed
+    end
+    # getter - reads
+    def breed
+        @breed
+    end
+    # setter - writes
+    def name=(name)
+        @name = name
+    end
+    # getter - reads
+    def name 
+        @name
+    end
 end
