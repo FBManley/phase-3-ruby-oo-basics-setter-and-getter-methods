7b925ef7fc5d6e8936027dae3d82d17331ac23fd
Jacob Manley <jbmanley45@gmail.com>
2022-09-06 11:13:32 -0700

    commit tree 1093d5edbccb9d368f2617b92ff3a4c65edbbc83

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    # setter - writes
+    def breed=(breed)
+        @breed = breed
+    end
+    # getter - reads
+    def breed
+        @breed
+    end
 
+    def name=(name)
+        @name = name
+    end
+
+    def name 
+        @name
+    end
 end
